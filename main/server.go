16916bc656a3eb743a5a125cd87cb94178dfc59b
goliatone <burgosemi@gmail.com>
2020-07-01 23:48:03 -0700

    add database setup

--- main/server.go
+++ main/server.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"sensors"
 	"sensors/config"
 	"sensors/rest"
+	"sensors/storage/postgres"
 
 	"github.com/gofiber/fiber"
 )
@@ -15,15 +18,19 @@ func main() {
 	cfg := config.ReadYaml("")
 	confg := sensors.GetConfig(*cfg)
 
+	database, err := postgres.NewDatabase(confg)
+	if err != nil {
+		log.Printf("database err %s", err)
+		os.Exit(1)
+	}
+
+	// postgres.Migrate(database)
+
 	server := fiber.New()
 
 	rest.Router(server)
 
-	server.Get("/", func(c *fiber.Ctx) {
-		c.Send("Hello World!")
-	})
-
-	port := fmt.Sprintf(":%v", 8372)
+	port := fmt.Sprintf(":%s", confg.Server.Port)
 	log.Fatal(server.Listen(port))
 
 	fmt.Sprintf("app key: %s\n", confg.Secret)
