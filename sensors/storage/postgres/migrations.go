36a8b2bbad0508c24e764b13909ea23559f134d5
goliatone <burgosemi@gmail.com>
2020-07-12 16:16:04 -0700

    update migrations;

--- sensors/storage/postgres/migrations.go
+++ sensors/storage/postgres/migrations.go
@@ -1,8 +1,17 @@
 package postgres
 
-import "sensors/storage"
+import (
+	"sensors/device"
+	"sensors/storage"
+)
 
 //Migrate updates he db with new columns, tables and the like
 func Migrate(database *storage.Database) {
 
+	//Enable uuid extension
+	database.DB.Exec(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`)
+
+	database.DB.AutoMigrate(
+		device.Device{},
+	)
 }
