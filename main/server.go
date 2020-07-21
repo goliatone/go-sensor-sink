d187ff9d877210ad84cbcb7a6e88e5c3d729ee61
goliatone <burgosemi@gmail.com>
2020-07-20 21:28:33 -0700

    clean up server

--- main/server.go
+++ main/server.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
 	"strings"
-	"time"
-
-	ikisocket "github.com/antoniodipinto/ikisocket"
 
 	"sensors"
 	"sensors/config"
 	"sensors/event"
 	"sensors/pubsub"
+	"sensors/realtime"
 	"sensors/rest"
 	"sensors/sink"
 	"sensors/storage/postgres"
@@ -21,46 +18,6 @@ import (
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/gofiber/fiber"
 )
-
-type WsManager struct {
-	clients map[string]string
-	sockets map[string]ikisocket.Websocket
-}
-
-func (ws *WsManager) AddSocket(socket ikisocket.Websocket) {
-	ws.sockets[socket.UUID] = socket
-}
-
-func (ws *WsManager) RemoveSocket(uuid string) {
-	delete(ws.sockets, uuid)
-}
-
-//Broadcast
-func (ws *WsManager) Broadcast(message []byte) {
-	for _, socket := range ws.sockets {
-		socket.EmitTo(socket.UUID, message)
-	}
-}
-
-type MessageObject struct {
-	Data string `json:"data"`
-	From string `json:"from"`
-	To   string `json:"to"`
-}
-
-func newReading(msg []byte) (sink.DHT22Reading, error) {
-	reading := sink.DHT22Reading{}
-	err := json.Unmarshal(msg, &reading)
-	if err != nil {
-		return sink.DHT22Reading{}, err
-	}
-
-	if reading.Time.IsZero() == true {
-		reading.Time = time.Now()
-	}
-
-	return reading, nil
-}
 
 const (
 	publicPath = "../frontend/src/build"
@@ -79,70 +36,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	server := fiber.New()
 	emitter := event.NewEmitter()
 
+	rest.Router(server, database)
 	postgres.Migrate(database)
-
-	server := fiber.New()
+	realtime.Websockets(server)
 
 	server.Static("/", publicPath)
-
-	wsm := WsManager{}
-	wsm.clients = make(map[string]string)
-	wsm.sockets = make(map[string]ikisocket.Websocket)
-
-	clients := make(map[string]string)
-	// sockets := make(map[string]ikisocket.Websocket)
-
-	server.Use(func(c *fiber.Ctx) {
-		c.Locals("user_id", c.Query("user_id"))
-		c.Next()
-	})
-
-	ikisocket.On(ikisocket.EventConnect, func(ep *ikisocket.EventPayload) {
-		fmt.Println("ws connect")
-		userID := fmt.Sprintf("%v", ep.Kws.Locals("user_id"))
-		wsm.sockets[userID] = ep.Kws
-	})
-
-	ikisocket.On(ikisocket.EventMessage, func(ep *ikisocket.EventPayload) {
-		fmt.Printf("ws message-> name %s data %s uuid %s \n", ep.Name, string(ep.Data), ep.SocketUUID)
-	})
-
-	ikisocket.On(ikisocket.EventDisconnect, func(ep *ikisocket.EventPayload) {
-		fmt.Println("fired disconnect" + ep.Error.Error())
-		userID := fmt.Sprintf("%v", ep.Kws.Locals("user_id"))
-		delete(wsm.sockets, userID)
-	})
-
-	server.Get("/ws", ikisocket.New(func(kws *ikisocket.Websocket) {
-		userID := fmt.Sprintf("%v", kws.Locals("user_id"))
-		fmt.Printf("ws user id: %s uuid: %s", userID, kws.UUID)
-
-		kws.SetAttribute("user_id", userID)
-
-		kws.OnConnect = func() {
-			clients[userID] = kws.UUID
-			// kws.Emit([]byte("Hello user " + userID))
-			// kws.Broadcast([]byte("User connected "+userID+" UUID: "+kws.UUID), true)
-		}
-
-		kws.OnMessage = func(data []byte) {
-			message := MessageObject{}
-			json.Unmarshal(data, &message)
-
-			fmt.Printf("message to %s: %s\n", message.To, message.Data)
-
-			err := kws.EmitTo(clients[message.To], data)
-			if err != nil {
-				fmt.Printf("message %s error: %v\n", message.From, err)
-			}
-		}
-	}))
-
-	ikisocket.On("close", func(payload *ikisocket.EventPayload) {
-		fmt.Println("ws closed " + payload.SocketAttributes["user_id"])
-	})
 
 	//move to package
 	sinkRepo := sink.NewRepository(database)
@@ -156,7 +57,7 @@ func main() {
 			return
 		}
 
-		reading, err := newReading(msg.Payload())
+		reading, err := sink.NewDHT22Reading(msg.Payload())
 		if err != nil {
 			log.Println("error handling reading:" + err.Error())
 			return
@@ -178,11 +79,10 @@ func main() {
 		}
 
 		if message, err := reading.Deserialize(); err == nil {
-			wsm.Broadcast(message)
+			// wsm.Broadcast(message)
+			realtime.Broadcast(message)
 		}
 	})
-
-	rest.Router(server, database)
 
 	port := fmt.Sprintf(":%s", cnf.Server.Port)
 	log.Fatal(server.Listen(port))
