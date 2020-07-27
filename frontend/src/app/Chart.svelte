f386cab74605f6c54b1a16adec6ce50828e163a9
goliatone <burgosemi@gmail.com>
2020-07-26 22:13:21 -0700

    merge humidty and temp in a single graph

--- frontend/src/app/Chart.svelte
+++ frontend/src/app/Chart.svelte
@@ -5,15 +5,10 @@
     import 'chartjs-plugin-streaming';
     import Chart from 'chart.js';
 
-    export let chartId;
-    export let label;
-    export let borderColor; 
-    export let backgroundColor;
-    //for development only!
     export let chart;
+    export let chartId;
 
     let ctx;
-    // let chart;
     let type;
     let unit;
 
@@ -28,59 +23,6 @@
                 duration: 0
             },
             scales: {
-                // xAxes: [{
-                //     type: 'time',
-                //     distribution: 'linear',//data are spread according to their time (distances can vary)
-                //     distribution: 'series',//data are spread at the same distance from each other
-                //     // bounds: 'ticks',
-                //     bounds: 'data',
-                //     time: {
-                //         unit: 'second',
-                //         displayFormats: {
-                //             quarter: 'h:mm:ss a'
-                //         }
-                //     },
-                //     offset: true,
-                //     ticks: {
-                //         major: {
-                //             enabled: true,
-                //             fontStyle: 'bold'
-                //         },
-                //         source: 'data',
-                //         // source: 'labels',
-                //         // source: 'auto',
-                //         autoSkip: true,
-                //         autoSkipPadding: 75,
-                //         maxRotation: 0,
-                //         sampleSize: 100
-                //     },
-                //     afterBuildTicks: function(scale, ticks) {
-                //         var majorUnit = scale._majorUnit;
-                //         var firstTick = ticks[0];
-                //         var i, ilen, val, tick, currMajor, lastMajor;
-
-                //         val = moment(ticks[0].value);
-                //         if ((majorUnit === 'minute' && val.second() === 0)
-                //                 || (majorUnit === 'hour' && val.minute() === 0)
-                //                 || (majorUnit === 'day' && val.hour() === 9)
-                //                 || (majorUnit === 'month' && val.date() <= 3 && val.isoWeekday() === 1)
-                //                 || (majorUnit === 'year' && val.month() === 0)) {
-                //             firstTick.major = true;
-                //         } else {
-                //             firstTick.major = false;
-                //         }
-                //         lastMajor = val.get(majorUnit);
-
-                //         for (i = 1, ilen = ticks.length; i < ilen; i++) {
-                //             tick = ticks[i];
-                //             val = moment(tick.value);
-                //             currMajor = val.get(majorUnit);
-                //             tick.major = currMajor !== lastMajor;
-                //             lastMajor = currMajor;
-                //         }
-                //         return ticks;
-                //     }
-                // }],
                 xAxes: [{
 						type: 'realtime',
 						realtime: {
@@ -93,11 +35,7 @@
                 yAxes: [{
                     gridLines: {
                         drawBorder: false
-                    },
-                    // scaleLabel: {
-                    //     display: true,
-                    //     labelString: scaleLabel
-                    // }
+                    }
                 }]
             },
             tooltips: {
@@ -105,15 +43,18 @@
                 mode: 'index',
                 callbacks: {
                     label: function(tooltipItem, myData) {
-                        var label = myData.datasets[tooltipItem.datasetIndex].label || '';
-                        if (label) {
-                            label += ': ';
-                        }
+                        let label = myData.datasets[tooltipItem.datasetIndex].label || '';
+                        if (label) label += ': ';
                         label += parseFloat(tooltipItem.value).toFixed(2);
                         return label;
                     }
                 }
-            }
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: false
+            },
+            plugins: {}
         }
     };
 
@@ -146,7 +87,7 @@
         // var type = document.getElementById('type').value;
         // dataset.type = type;
 
-        chart.config.data.datasets.forEach((dataset, i)=>{
+        chart.config.data.datasets.forEach((dataset, i) => {
             dataset.data = data[i];
         });
 
@@ -156,7 +97,7 @@
     afterUpdate(initializeChart);
 
     $: if(chart && type) {
-        chart.config.data.datasets.forEach(dataset=>{
+        chart.config.data.datasets.forEach(dataset => {
             dataset.type = type;
         });
         
