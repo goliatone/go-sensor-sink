5571be97cebd72fe68124479bd375b73de3d81ba
goliatone <burgosemi@gmail.com>
2020-07-26 22:11:59 -0700

    merge humidty and temp in a single graph

--- frontend/src/app/App.svelte
+++ frontend/src/app/App.svelte
@@ -1,19 +1,52 @@
 <script>
+	import {onMount} from 'svelte';
 	import Chart from './Chart.svelte';
 	import moment from 'moment';
+	import KalmanFilter from 'kalmanjs';
+	import Api from './services/api';
 	import makeWebsocketStore from './stores/ws';
 
-	let tChart;
-	let hChart;
+	let api = new Api();
+	api.get();
+	let chart;
 	let initialValue = {};
+
+	let datasets = [{
+		label:'Temperature °C',
+		borderColor:'rgb(255, 99, 132)',
+		backgroundColor:'rgb(255, 159, 64)',
+		data:[],
+		type: 'line',
+		pointRadius: 0,
+		fill: false,
+		lineTension: 0,
+		borderWidth: 2
+	},{
+		label:'Humidity %',
+		borderColor:'rgb(54, 162, 235)',
+		backgroundColor:'rgb(153, 102, 255)',
+		data:[],
+		type: 'line',
+		pointRadius: 0,
+		fill: false,
+		lineTension: 0,
+		borderWidth: 2
+	}];
+	
+	var kfilter = new KalmanFilter({R: 0.01, Q: 3});
 
 	export let version;
 	export let environment;
 	
 	function makeWsUrl() {
 		const qs = new URLSearchParams(location.search);
-        const userId = qs.get('user_id');
-		let url = `ws://localhost:3131/ws?user_id=${userId}`;
+		//TODO: This should come form template renderer, not user!
+		const userId = qs.get('user_id');
+		const host = qs.get('wshost') ? qs.get('wshost') : location.host;
+		const protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+		
+		const url = `${protocol}://${host}/ws?user_id=${userId}`;
+
 		console.log('url', url);
 		return url;
 	}
@@ -21,29 +54,54 @@
 	const wsStore = makeWebsocketStore(makeWsUrl(),[],  initialValue);
 	
 
-	let tData = [{t: 1595219532618, y: 27 }];
-	let hData = [{t: 1595219532618, y: 27 }];
+	let data = [
+		[],
+		[]
+	];
+	
+
+	let tAvg = {
+		sum: 0,
+		tot: 0,
+		add(v) {
+			this.tot += 1;
+			this.sum = Math.round((this.sum + v) / Math.min(this.tot, 2));
+			return this.sum;
+		}
+	};
+
+	let hAvg = {
+		add(v) {
+			return Math.round(kfilter.filter(v));
+		}
+	};
 
 	function maxPush(a, v, max=6) {
   		a.push(v);
-  		if(a.length >= max) a.shift();
-  		return a
+  		if(a.length > max){ 
+			a.shift();
+		}
 	}
+
 	wsStore.subscribe(value => {
-		if(!value || !value.time || !tChart) return;
+		if(!value || !value.time || !chart) return;
 
-		maxPush(tData, {
+		//TODO: Should make average for each device, maybe use kalman filter
+		maxPush(data[0], {
 			t: parseInt(Math.abs(moment(value.time).format('x'))),
-            y: value.t
-		}, 100);
+            y: tAvg.add(value.t)
+		}, 50);
 
-		maxPush(hData, {
+		maxPush(data[1], {
 			t: parseInt(Math.abs(moment(value.time).format('x'))),
-            y: value.h
-		}, 100);
+            y: hAvg.add(value.h)
+		}, 50);
 
-		tChart.updateData(tData);
-		hChart.updateData(hData);
+		chart.updateData(data);
+	});
+
+	onMount(_=>{
+		window.chart = chart;
 	});
 </script>
 
@@ -52,19 +110,9 @@
 	<p>Simple dashboard showing data collected using ESP8266 and DHT22 sensors.</p>
 	
 	<Chart 
-		chartId={"tChart"} 
-		label="Temperature °C"
-		borderColor="rgb(255, 99, 132)" 
-		backgroundColor="rgb(255, 159, 64)" 
-		bind:this={tChart}
-	/>
-
-	<Chart 
-		chartId={"hChart"} 
-		label="Humidity %"
-		borderColor="rgb(54, 162, 235)" 
-		backgroundColor="rgb(153, 102, 255)" 
-		bind:this={hChart}
+		chartId={"chart"} 
+		{datasets}
+		bind:this={chart}
 	/>
 
 	<footer>
