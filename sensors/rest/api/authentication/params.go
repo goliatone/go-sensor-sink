f28de2ecc88dd7b81beecbc43204df25225bb3e0
goliatone <burgosemi@gmail.com>
2020-07-31 17:14:17 -0700

    add auth flow

---
+++ sensors/rest/api/authentication/params.go
@@ -0,0 +1,42 @@
+package authentication
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+//LoginParams parameters sent in login request
+type LoginParams struct {
+	Identifier string `json:"identifier"`
+	Password   string `json:"password"`
+}
+
+//RegistrationParams parameters sent in to register request
+type RegistrationParams struct {
+	Username string `json:"username"`
+	Email    string `json:"email`
+	Password string `json:"password"`
+}
+
+//ValidateRegisterParams validates registration parameters
+func ValidateRegisterParams(params *RegistrationParams) (bool, error) {
+
+	var pMap map[string]interface{}
+	in, _ := json.Marshal(params)
+	_ = json.Unmarshal(in, &pMap)
+
+	var err error
+	for k, v := range pMap {
+		if v == "" {
+			log.Printf("checking %v value passed %v\n", k, v)
+			err = ErrInvalidParams{message: fmt.Sprintf("%v is required", k)}
+			break
+		}
+	}
+
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
