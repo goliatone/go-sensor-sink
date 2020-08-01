f17003068168bd10a7b11210ffbae2816064f0b6
goliatone <burgosemi@gmail.com>
2020-07-31 17:19:36 -0700

    add errors to auth

--- sensors/auth/errors.go
+++ sensors/auth/errors.go
@@ -5,6 +5,10 @@ type ErrUnexpected struct {
 	debug string
 }
 
+func (err ErrUnexpected) Error() string {
+	return err.debug
+}
+
 //NewErrUnexpected creates a new ErrUnexpected DB unexpected error
 func NewErrUnexpected(err error) *ErrUnexpected {
 	return &ErrUnexpected{
@@ -26,4 +30,32 @@ func NewErrUserNotFound(message string) *ErrUserNotFound {
 	return &ErrUserNotFound{
 		message: message,
 	}
+}
+
+//ErrUnauthorized 401 error
+type ErrUnauthorized struct {
+	Message string
+}
+
+//Error string
+func (err ErrUnauthorized) Error() string {
+	return err.Message
+}
+
+// ErrTokenParsing ...
+type ErrTokenParsing struct {
+	message string
+}
+
+func (err ErrTokenParsing) Error() string {
+	return err.message
+}
+
+type ErrHashPassword struct {
+	password string
+	message  string
+}
+
+func (err ErrHashPassword) Error() string {
+	return err.message
 }
