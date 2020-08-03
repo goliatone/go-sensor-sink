ed3babccce2bd6375244a56538b9d167ff85c3fc
goliatone <burgosemi@gmail.com>
2020-08-02 22:55:50 -0700

    add support for fork to server

--- main/server.go
+++ main/server.go
@@ -39,50 +39,54 @@ func main() {
 	postgres.Migrate(database)
 
 	server := fiber.New()
+	server.Settings.Prefork = true
+
 	emitter := event.NewEmitter()
 	domain := registry.NewDomain(cnf, database)
 
 	rest.Router(server, domain, cnf)
 	realtime.Websockets(server)
-	pubsub.SetConfig(&cnf.Mqtt)
-
 	//move to package
 	server.Static("/", publicPath)
 
-	mqttClient := pubsub.AddCommandHandler(0, func(mc mqtt.Client, msg mqtt.Message) {
-		//TODO: We get all messages, we should actually prefix it with server id so
-		//that we don't get our own messages...
-		// log.Printf("Reading message: %s %s", msg.Topic(), msg.Payload())
-		if strings.Contains(msg.Topic(), "/reading") == false {
-			return
+	if server.IsChild() == false {
+		pubsub.SetConfig(&cnf.Mqtt)
+
+		mqttClient := pubsub.AddCommandHandler(0, func(mc mqtt.Client, msg mqtt.Message) {
+			//TODO: We get all messages, we should actually prefix it with server id so
+			//that we don't get our own messages...
+			// log.Printf("Reading message: %s %s", msg.Topic(), msg.Payload())
+			if strings.Contains(msg.Topic(), "/reading") == false {
+				return
+			}
+
+			reading, err := sink.NewDHT22Reading(msg.Payload())
+			if err != nil {
+				log.Println("error handling reading:" + err.Error())
+				return
+			}
+
+			emitter.EmitSync("mqtt.event", reading)
+		})
+
+		if mqttClient != nil {
+			log.Println("started mqtt")
 		}
 
-		reading, err := sink.NewDHT22Reading(msg.Payload())
-		if err != nil {
-			log.Println("error handling reading:" + err.Error())
-			return
-		}
+		emitter.On("mqtt.event", func(args ...interface{}) {
+			reading := args[0].(sink.DHT22Reading)
+			_, err = domain.Readings.Create(reading)
 
-		emitter.EmitSync("mqtt.event", reading)
-	})
+			if err != nil {
+				log.Println("error adding reading:" + err.Error())
+			}
 
-	if mqttClient != nil {
-		log.Println("started mqtt")
+			if message, err := reading.Deserialize(); err == nil {
+				// wsm.Broadcast(message)
+				realtime.Broadcast(message)
+			}
+		})
 	}
-
-	emitter.On("mqtt.event", func(args ...interface{}) {
-		reading := args[0].(sink.DHT22Reading)
-		_, err = domain.Readings.Add(reading)
-
-		if err != nil {
-			log.Println("error adding reading:" + err.Error())
-		}
-
-		if message, err := reading.Deserialize(); err == nil {
-			// wsm.Broadcast(message)
-			realtime.Broadcast(message)
-		}
-	})
 
 	port := fmt.Sprintf(":%s", cnf.Server.Port)
 	log.Fatal(server.Listen(port))
