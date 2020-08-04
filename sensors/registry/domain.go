a2d577fd1d121a3c51daf36279bc58e6d09008b0
goliatone <burgosemi@gmail.com>
2020-08-03 21:11:43 -0700

    implement users interactor

--- sensors/registry/domain.go
+++ sensors/registry/domain.go
@@ -6,26 +6,28 @@ import (
 	"sensors/device"
 	"sensors/sink"
 	"sensors/storage"
+	"sensors/user"
 )
 
 //Domain exposes our actions
 type Domain struct {
 	Auth     auth.Interactor
-	Users    auth.Repository
+	Users    user.Interactor
 	Devices  device.Interactor
 	Readings sink.Interactor
 }
 
 //NewDomain will create a new domain
-func NewDomain(config sensors.Config, database *storage.Database) *Domain {
-	userRepo := auth.NewRepository(database)
+func NewDomain(config sensors.Config, database *storage.Database, channels *Channels) *Domain {
+	authRepo := auth.NewRepository(database)
 	sinkRepo := sink.NewRepository(database)
+	userRepo := user.NewRepository(database)
 	deviceRepo := device.NewRepository(database)
 
 	return &Domain{
-		Users:    userRepo,
+		Users:    user.NewInteractor(userRepo),
 		Readings: sink.NewInteractor(sinkRepo),
 		Devices:  device.NewInteractor(deviceRepo),
-		Auth:     auth.NewInteractor(config.Auth, userRepo),
+		Auth:     auth.NewInteractor(config.Auth, authRepo, channels.ChannelNewUsers),
 	}
 }
