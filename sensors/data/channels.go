11c4ef15c9d771a3c6be60db7ec9986b1d20e2c3
goliatone <burgosemi@gmail.com>
2020-08-03 21:12:33 -0700

    add channels

--- sensors/data/channels.go
+++ sensors/data/channels.go
@@ -1,5 +1,19 @@
 package data
 
+import uuid "github.com/satori/go.uuid"
+
 //SensorReading holds sensor data
 type SensorReading struct {
+}
+
+//UserContract for User operations
+type UserContract struct {
+	UserID uuid.UUID
+}
+
+//ChanNewUsers handles new users
+type ChanNewUsers struct {
+	Channel chan UserContract
+	Reader  <-chan UserContract
+	Writer  chan<- UserContract
 }
